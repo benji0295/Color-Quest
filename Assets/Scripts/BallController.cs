ef75647ae1098de1ad0ab44b325e7d8a3310895a
Ben Smith <ben.c.smith95@gmail.com>
2024-10-20 22:17:16 -0400

    Updated level design

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,32 +5,42 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
 
-  private void Start()
+  private bool isRedComplete = false;
+  private bool isBlueComplete = false;
+  private bool isGreenComplete = false;
+  private bool isYellowComplete = false;
+  private void Update()
   {
-
+    if (isRedComplete && isBlueComplete && isGreenComplete && isYellowComplete)
+    {
+      Debug.Log("Game Over.");
+    }
   }
   private void OnTriggerEnter(Collider other)
   {
     if (gameObject.CompareTag("RedBall") && other.CompareTag("RedCorner"))
     {
       Debug.Log("Red complete.");
+      isRedComplete = true;
     }
     else if (gameObject.CompareTag("BlueBall") && other.CompareTag("BlueCorner"))
     {
       Debug.Log("Blue complete.");
+      isBlueComplete = true;
     }
     else if (gameObject.CompareTag("GreenBall") && other.CompareTag("GreenCorner"))
     {
       Debug.Log("Green complete.");
+      isGreenComplete = true;
     }
     else if (gameObject.CompareTag("YellowBall") && other.CompareTag("YellowCorner"))
     {
       Debug.Log("Yellow complete.");
+      isYellowComplete = true;
     }
     else
     {
       Debug.Log("Wrong corner.");
     }
-
   }
 }
