0504b1c82678bb74141b3a3f17fd0a0d0d382811
Ben Smith <ben.c.smith95@gmail.com>
2024-10-27 19:50:32 -0400

    Updated random ball generator

--- Assets/Scripts/BallGenerator.cs
+++ Assets/Scripts/BallGenerator.cs
@@ -10,21 +10,28 @@ public class BallGenerator : MonoBehaviour
   public GameObject greenBall;
   public GameObject yellowBall;
   public float maxDistanceFromGenerator = 10.0f;
-  public float minDistanceFromGenerator = 3.0f;
+  public float minDistanceFromGenerator = 5.0f;
   public float height = 24.0f;
 
   private void Start()
   {
-    var redPosition = Random.Range(minDistanceFromGenerator, maxDistanceFromGenerator);
-    var yellowPosition = Random.Range(minDistanceFromGenerator, maxDistanceFromGenerator);
-    var greenPosition = Random.Range(minDistanceFromGenerator, maxDistanceFromGenerator);
-    var bluePosition = Random.Range(minDistanceFromGenerator, maxDistanceFromGenerator);
+    var redPosition = RandomPosition();
+    var yellowPosition = RandomPosition();
+    var greenPosition = RandomPosition();
+    var bluePosition = RandomPosition();
 
-    Instantiate(redBall, new Vector3(redPosition, height, redPosition), Quaternion.identity);
-    Instantiate(blueBall, new Vector3(-bluePosition, height, bluePosition), Quaternion.identity);
-    Instantiate(greenBall, new Vector3(-greenPosition, height, -greenPosition), Quaternion.identity);
-    Instantiate(yellowBall, new Vector3(yellowPosition, height, -yellowPosition), Quaternion.identity);
+    Instantiate(redBall, redPosition, Quaternion.identity);
+    Instantiate(yellowBall, yellowPosition, Quaternion.identity);
+    Instantiate(greenBall, greenPosition, Quaternion.identity);
+    Instantiate(blueBall, bluePosition, Quaternion.identity);
 
   }
+  private Vector3 RandomPosition()
+  {
+    Vector2 direction = Random.insideUnitCircle.normalized;
+    float distance = Random.Range(minDistanceFromGenerator, maxDistanceFromGenerator);
+    Vector3 position = new Vector3(direction.x * distance, height, direction.y * distance);
 
+    return position;
+  }
 }
